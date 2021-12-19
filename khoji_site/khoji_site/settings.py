6f4f4d56eb3053675f475f6fa0010984d93f38dc
khojiT <a.khoji2001@gmail.com>
2021-12-19 11:20:33 +0330

    docker

--- khoji_site/khoji_site/settings.py
+++ khoji_site/khoji_site/settings.py
@@ -125,13 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = BASE_DIR.parent / 'static'
+
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR.parent / 'media'
 
 
 # Default primary key field type
